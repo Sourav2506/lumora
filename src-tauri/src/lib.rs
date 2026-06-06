7a49dbf2993b04f6eff55c1d98f4c71fde9c48a2
Sourav Suna <souravsuna@gmail.com>
2026-06-06 18:25:05 +0530

    feat: implement system tray support

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,10 +1,8 @@
 use tauri::{
     menu::{Menu, MenuItem},
-    tray::{TrayIconBuilder, TrayIconEvent},
+    tray::TrayIconBuilder,
     Manager,
-    WindowEvent,
 };
-
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
@@ -12,36 +10,91 @@ pub fn run() {
         .plugin(tauri_plugin_store::Builder::default().build())
         .plugin(tauri_plugin_notification::init())
         .setup(|app| {
-            let quit =
-                MenuItem::with_id(
-            app,
-                "quit",
-                "Quit",
-            true,
-            None::<&str>,
+            let show = MenuItem::with_id(
+                app,
+                "show",
+                "Show Lumora",
+                true,
+                None::<&str>,
             )?;
 
-            let menu =
-                Menu::with_items(
-            app,
-            &[&quit],
-                )?;
+            let quit = MenuItem::with_id(
+                app,
+                "quit",
+                "Quit",
+                true,
+                None::<&str>,
+            )?;
 
-            let _tray =
-                TrayIconBuilder::new()
-                    .icon(app.default_window_icon().unwrap().clone())
-                    .menu(&menu)
-                    .on_menu_event(|app, event| {
-                        if event.id().as_ref() == "quit" {
+            let menu = Menu::with_items(
+                app,
+                &[&show, &quit],
+            )?;
+
+            let app_handle = app.handle().clone();
+
+            let _tray = TrayIconBuilder::new()
+                .icon(
+                    app.default_window_icon()
+                        .unwrap()
+                        .clone(),
+                )
+                .menu(&menu)
+                .on_menu_event(move |app, event| {
+                    match event.id().as_ref() {
+                        "show" => {
+                            if let Some(window) =
+                                app.get_webview_window("main")
+                            {
+                                let _ = window.show();
+                                let _ = window.set_focus();
+                            }
+                        }
+
+                        "quit" => {
                             app.exit(0);
                         }
-                    })
-                    .build(app)?;
-            
+
+                        _ => {}
+                    }
+                })
+                .build(app)?;
+
+            if let Some(window) =
+                app_handle.get_webview_window("main")
+            {
+                let app_clone =
+                    app_handle.clone();
+
+                window.on_window_event(
+                    move |event| {
+                        if let tauri::WindowEvent::CloseRequested {
+                            api,
+                            ..
+                        } = event
+                        {
+                            api.prevent_close();
+
+                            if let Some(window) =
+                                app_clone
+                                    .get_webview_window(
+                                        "main",
+                                    )
+                            {
+                                let _ =
+                                    window.hide();
+                            }
+                        }
+                    },
+                );
+            }
+
             if cfg!(debug_assertions) {
                 app.handle().plugin(
                     tauri_plugin_log::Builder::default()
-                        .level(log::LevelFilter::Info)
+                        .level(
+                            log::LevelFilter::Info,
+                        )
                         .build(),
                 )?;
             }
@@ -49,5 +102,7 @@ pub fn run() {
             Ok(())
         })
         .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+        .expect(
+            "error while running tauri application",
+        );
 }
