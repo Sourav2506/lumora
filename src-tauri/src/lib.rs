9d0fd1c4ff1ee1a0ec72ce96e014d70f142fea1d
Sourav Suna <souravsuna@gmail.com>
2026-06-07 08:47:13 +0530

    feat: add startup preference persistence

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -7,6 +7,12 @@ use tauri::{
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(
+            tauri_plugin_autostart::init(
+                tauri_plugin_autostart::MacosLauncher::LaunchAgent,
+                None,
+            ),
+        )
         .plugin(tauri_plugin_store::Builder::default().build())
         .plugin(tauri_plugin_notification::init())
         .setup(|app| {
@@ -81,8 +87,7 @@ pub fn run() {
                                         "main",
                                     )
                             {
-                                let _ =
-                                    window.hide();
+                                let _ = window.hide();
                             }
                         }
                     },
